4cc849f918e7069b332002dd281f5bc6e157518a
Casthegrinni <brunno.castigrini@bandtec.com.br>
2021-07-31 19:02:57 -0300

    dictionaries

---
+++ dictionaries.swift
@@ -0,0 +1,17 @@
+func exercise() {
+
+    var stockTickers: [String: String] = [
+        "APPL" : "Apple Inc", 
+    	"HOG": "Harley-Davidson Inc", 
+    	"BOOM": "Dynamic Materials", 
+    	"HEINY": "Heineken", 
+    	"BEN": "Franklin Resources Inc",
+        "WORK": "Slack Technologies Inc"
+    ]
+    
+    print(stockTickers["WORK"]!)
+    print(stockTickers["BOOM"]!)
+}
+
+exercise()
+ 
